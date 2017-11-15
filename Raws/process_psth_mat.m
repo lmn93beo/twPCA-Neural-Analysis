7f6fecfa97dc543ee93ad40166d8c022cda32046
Nhat Le <lmn93beo@yahoo.com>
2017-11-15 15:47:36 -0500

    MATLAB GUI for visualizing stretches

---
+++ Raws/process_psth_mat.m
@@ -0,0 +1,45 @@
+load XAcortexHand.mat;
+
+shorts = X.XS;
+longs = X.XL;
+
+%% Collect responses for short condition
+N = numel(shorts);
+
+% Find longest element
+longest = 0;
+for i = 1 : N
+    if numel(shorts{i}) > longest
+        longest = numel(shorts{i});
+    end
+end
+
+shorts_all_mat = nan(N, longest);
+for i = 1 : N
+    elem_len = numel(shorts{i});
+    shorts_all_mat(i, 1:elem_len) = shorts{i}; 
+end
+
+% Save as .csv file
+csvwrite('psth_short_XAcortexHand_171109.csv', shorts_all_mat);
+
+%% Collect responses for long condition
+N = numel(longs);
+
+% Find longest element
+longest = 0;
+for i = 1 : N
+    if numel(longs{i}) > longest
+        longest = numel(longs{i});
+    end
+end
+
+longs_all_mat = nan(N, longest);
+for i = 1 : N
+    elem_len = numel(longs{i});
+    longs_all_mat(i, 1:elem_len) = longs{i}; 
+end
+
+% Save as .csv file
+csvwrite('psth_long_XAcortexHand_171109.csv', longs_all_mat);
+
--- alignment_batch/alignment_batch.m
+++ alignment_batch/alignment_batch.m
@@ -22,7 +22,7 @@ function varargout = alignment_batch(varargin)
 
 % Edit the above text to modify the response to help alignment_batch
 
-% Last Modified by GUIDE v2.5 08-Nov-2017 16:36:43
+% Last Modified by GUIDE v2.5 09-Nov-2017 15:40:21
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -57,12 +57,8 @@ handles.output = hObject;
 
 global params
 % Set up global parameters (constants)
-params.t_points_short = 7;
-params.t_points_long = 9;
-params.n_neurons = 105;
 params.dt_bin = 40; %ms
-params.times = [740, 760, 780, 800, 820, 840, 860, 1300, ...
-    1340, 1380, 1420, 1460, 1500, 1540, 1580, 1620];
+
 params.palette = [0.87021914648212229, 0.92132256824298342, 0.9685044213763937;
  0.77524029219530954, 0.85830065359477115, 0.93682429834678971;
  0.61725490196078436, 0.79086505190311418, 0.88184544405997689;
@@ -81,21 +77,45 @@ params.palette = [0.87021914648212229, 0.92132256824298342, 0.9685044213763937;
  0.59461745482506734, 0.046136101499423293, 0.075586312956555157];
 
 % Load unaligned data
-short_psth = csvread('../psth_short_all_neurons_nans_171102.csv');
-long_psth = csvread('../psth_long_all_neurons_nans_171102.csv');
+[shortfilename, shortpath] = uigetfile('../Raws/*.csv', ...
+                'Pick a file for short PSTH');
+[longfilename, longpath] = uigetfile('../Raws/*.csv', ...
+                'Pick a file for long PSTH');
+short_psth = csvread([shortpath shortfilename]);
+long_psth = csvread([longpath longfilename]);
 
+params.t_points_short = str2double(get(handles.tshort_txt, 'String'));
+params.t_points_long = str2double(get(handles.tlong_txt, 'String'));
+params.n_neurons = str2double(get(handles.nneurons_txt, 'String'));
+tshort = size(short_psth, 2);
+tlong = size(long_psth, 2);
+
+if params.t_points_short == 6
+    params.times = [740, 760, 780, 800, 820, 840, 1300, ...
+        1340, 1380, 1420, 1460, 1500, 1540, 1580, 1620];
+else
+    params.times = [740, 760, 780, 800, 820, 840, 860, 1300, ...
+    1340, 1380, 1420, 1460, 1500, 1540, 1580, 1620];
+end
+
+% Pad short psth with nans and combine
 reshaped_short_psth = reshape(short_psth, params.t_points_short, ...
     params.n_neurons, []);
 reshaped_long_psth = reshape(long_psth, params.t_points_long, ...
     params.n_neurons, []);
 reshaped_short_pad = padarray(reshaped_short_psth, ...
-    [0, 0, 46 - 25], nan, 'post');
+    [0, 0, tlong - tshort], nan, 'post');
 params.reshaped_all_pad = cat(1, reshaped_short_pad, reshaped_long_psth);
 
-% Pad short psth with nans and combine
+% Load the objective scores
+[scorefile, scorepath] = uigetfile('../Processed_neurons/*.csv', ...
+            'Choose a file for objective scores');
+params.scores = csvread([scorepath scorefile]);
 
-
-
+% Neuron file path
+[params.neuronfilename, params.neuronfilepath] = uigetfile('../Processed_neurons/*.csv', ...
+                'Picked a warp file');
+            
 % Update handles structure
 guidata(hObject, handles);
 
@@ -126,12 +146,21 @@ global params
 id = min(floor(get(hObject, 'Value')), 105);
 
 % Load the aligned activity
-align_filename = ['../neuron' num2str(id) '_init_linear_warp_affine_recons.csv'];
+base_filename = [params.neuronfilepath params.neuronfilename];
+id_pos = regexp(base_filename, '[0-9]');
+id_pos = id_pos(1);
+warp_filename = [base_filename(1:(id_pos - 1)) num2str(id) ...
+    base_filename((id_pos + 1): end)];
+warp_pos = strfind(warp_filename, 'warp');
+warp_pos = warp_pos(end);
+align_filename = [warp_filename(1:(warp_pos - 1)) 'recons' ...
+    warp_filename((warp_pos + 4):end)];
+    
 aligned = csvread(align_filename);
 
 % Loop through with the appropriate color
 axes(handles.align_axes);
-for i = 1:16
+for i = 1:(params.t_points_short + params.t_points_long)
     plot((1:size(aligned, 2)) * 40, aligned(i,:), ...
         'Color', params.palette(i,:));
     hold on;
@@ -139,10 +168,9 @@ end
 hold off;
 xlabel('Time (ms)');
 ylabel('Normalized PSTH');
-title('PSTH after alignment')
+title('After alignment')
 
 % Plot all warp factors
-warp_filename = ['../neuron' num2str(id) '_init_linear_warp_affine_warp2.csv'];
 warpN = csvread(warp_filename);
 slope = size(warpN, 2)./(warpN(:,end) - warpN(:,1));
 axes(handles.warp_axes);
@@ -151,12 +179,11 @@ xlim([0, 1700]);
 ylim([0, 1.2]);
 xlabel('Time (ms)');
 ylabel('Stretch');
-title('Warps at different trial lengths')
+title('Warp factor')
 
 % Plot the original data
 axes(handles.original_axes);
-size(params.reshaped_all_pad)
-for i = 1:16
+for i = 1:(params.t_points_short + params.t_points_long)
     plot(squeeze(params.reshaped_all_pad(i, id + 1, :)), ...
         'Color', params.palette(i,:));
     hold on;
@@ -164,10 +191,21 @@ end
 hold off;
 xlabel('Time (ms)');
 ylabel('PSTH');
-title('PSTH before alignment')
+title('Before alignment')
 
+% Plot the scores
+axes(handles.obj_axes);
+params.scores(params.scores < 0) = 0;
+stem(params.scores, 'filled');
+hold on;
+stem(id + 1, params.scores(id + 1), 'filled');
+hold off;
+xlim([0, numel(params.scores)]);
+%ylim([0, 0.1]);
 
+% Display parameters
 set(handles.neuronID_txt, 'String', num2str(id));
+set(handles.obj_txt, 'String', num2str(params.scores(id + 1)));
 guidata(hObject, handles);
 
 
@@ -182,3 +220,143 @@ function slider1_CreateFcn(hObject, eventdata, handles)
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
+
+
+
+function nneurons_txt_Callback(hObject, eventdata, handles)
+% hObject    handle to nneurons_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of nneurons_txt as text
+%        str2double(get(hObject,'String')) returns contents of nneurons_txt as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function nneurons_txt_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to nneurons_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function tshort_txt_Callback(hObject, eventdata, handles)
+% hObject    handle to tshort_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of tshort_txt as text
+%        str2double(get(hObject,'String')) returns contents of tshort_txt as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function tshort_txt_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to tshort_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function tlong_txt_Callback(hObject, eventdata, handles)
+% hObject    handle to tlong_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of tlong_txt as text
+%        str2double(get(hObject,'String')) returns contents of tlong_txt as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function tlong_txt_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to tlong_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function suffix_txt_Callback(hObject, eventdata, handles)
+% hObject    handle to suffix_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of suffix_txt as text
+%        str2double(get(hObject,'String')) returns contents of suffix_txt as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function suffix_txt_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to suffix_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in reload_button.
+function reload_button_Callback(hObject, eventdata, handles)
+% hObject    handle to reload_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global params
+
+% Load unaligned data
+[shortfilename, shortpath] = uigetfile('../Raws/*.csv', ...
+                'Pick a file for short PSTH');
+[longfilename, longpath] = uigetfile('../Raws/*.csv', ...
+                'Pick a file for long PSTH');
+short_psth = csvread([shortpath shortfilename]);
+long_psth = csvread([longpath longfilename]);
+
+params.t_points_short = str2double(get(handles.tshort_txt, 'String'));
+params.t_points_long = str2double(get(handles.tlong_txt, 'String'));
+params.n_neurons = str2double(get(handles.nneurons_txt, 'String'));
+tshort = size(short_psth, 2);
+tlong = size(long_psth, 2);
+
+if params.t_points_short == 6
+    params.times = [740, 760, 780, 800, 820, 840, 1300, ...
+        1340, 1380, 1420, 1460, 1500, 1540, 1580, 1620];
+else
+    params.times = [740, 760, 780, 800, 820, 840, 860, 1300, ...
+    1340, 1380, 1420, 1460, 1500, 1540, 1580, 1620];
+end
+
+% Pad short psth with nans and combine
+reshaped_short_psth = reshape(short_psth, params.t_points_short, ...
+    params.n_neurons, []);
+reshaped_long_psth = reshape(long_psth, params.t_points_long, ...
+    params.n_neurons, []);
+reshaped_short_pad = padarray(reshaped_short_psth, ...
+    [0, 0, tlong - tshort], nan, 'post');
+params.reshaped_all_pad = cat(1, reshaped_short_pad, reshaped_long_psth);
+
+% Load the objective scores
+[scorefile, scorepath] = uigetfile('../Processed_neurons/*.csv', ...
+            'Choose a file for objective scores');
+params.scores = csvread([scorepath scorefile]);
+
+% Neuron file path
+[params.neuronfilename, params.neuronfilepath] = uigetfile('../Processed_neurons/*.csv', ...
+                'Picked a warp file');
